9e5a6ab675d45622c54455d610cf4e9a7ad5b206
terrificmn <mildsm@gmail.com>
2021-04-06 06:55:06 +0900

    nothing changed but only title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
                     Do you want to become a developer?
+                    Yes, I do!
                 </h1>
                 
                 <a href="/blog" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
